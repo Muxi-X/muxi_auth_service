38d3402e22cbba85960455031ac27c8549232746
yuyilei <yyl1352091742@gmail.com>
2017-08-16 14:34:29 +0800

    update login_required

--- auth/auth/decorators.py
+++ auth/auth/decorators.py
@@ -9,7 +9,9 @@ def login_required(f) :
         token = request.headers.get('token')
         if token is not None :
             g.current_user = User.verify_auth_token(token)
-            return f(*args,**kwargs)
-        return jsonify("login first!") , 401
+            if g.current_user is not None :
+                return f(*args,**kwargs)
+            return jsonify({"message" :"not such user!"}) , 402
+        return jsonify({"message" :"login first!"}) , 401
     return decorated
 
