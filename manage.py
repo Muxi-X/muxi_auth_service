c71484c30a10f96d9f2358c1a6f03ab043287309
yuyilei <yyl1352091742@gmail.com>
2017-08-20 11:15:09 +0800

    add insert_roles

--- manage.py
+++ manage.py
@@ -107,6 +107,34 @@ def adduser():
     print "<user %s add in database>" % username
 
 @manager.command
+def insert_roles():
+    """
+    insert all Roles in command line
+    -------------------------------
+    User: write & comment
+    Moderator: write & comment & moderate_comments
+    Administrator: full permissions
+    """
+    roles = {
+        'User': (Permission.COMMENT | Permission.WRITE_ARTICLES, True),
+        'Moderator': (Permission.COMMENT |
+                      Permission.WRITE_ARTICLES |
+                      Permission.MODERATE_COMMENTS, False),
+        'Administrator': (0xff, False)
+    }
+    for r in roles:
+        role = Role.query.filter_by(name=r).first()
+        if role is None:
+            role = Role(name=r)
+        role.permissions = roles[r][0]
+        role.default = roles[r][1]
+        db.session.add(role)
+    db.session.commit()
+    print('roles has been inserted!')
+    sys.exit(0)
+
+
+@manager.command
 def test() :
     import unittest
     tests = unittest.TestLoader().discover('tests')
