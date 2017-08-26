639694186593d6b93660a51bcedd5eda33a1c0b2
yuyilei <yyl1352091742@gmail.com>
2017-08-26 14:09:03 +0800

    update login with  base64

--- auth/auth/login.py
+++ auth/auth/login.py
@@ -18,13 +18,16 @@ import base64
 def login():
     username = request.get_json().get("username")
     pwd = request.get_json().get("password")
-    pwd = base64.b64decode(pwd)
-    pwd = unicode(pwd)
-    if not username or not pwd :
-        return jsonify({}) , 402
+    try : 
+        pwd = base64.b64decode(pwd)
+        pwd = unicode(pwd)
+    except : 
+        return jsonify({ }) , 401 
+    if not username  :
+        return jsonify({}) , 40
     user = User.query.filter_by(username=username).first()
     if not user:
-        return jsonify({}), 403
+        return jsonify({}), 401
     if not user.verify_password(pwd):
         return jsonify({}), 400
 
